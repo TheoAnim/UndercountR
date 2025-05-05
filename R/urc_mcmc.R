21f23240a2bc14bc0e254456e02689a9a490b9a5
Theo Anim <theophilus.animbediako@jacks.sdstate.edu>
2025-05-05 10:50:55 -0500

    zip and negbin

--- R/urc_mcmc.R
+++ R/urc_mcmc.R
@@ -1,29 +1,40 @@
 #' @title poisson model
 #' @export
+#' @param data A named list of data to pass to JAGS
+#' @param parameters_poisson Character vector of parameters to save for Poisson model
+#'
+#' @return A list with model outputs and DICs
 
 urc_mcmc <- function(data) {
-  data$n_obs <- length(data$yobs)
-  data$n_valdata <- length(data$ystar)
-  file <- "underreported_poisson.jags"
-  parameters <- c("mu", "lambda", "p")
-
-  file_package <- system.file(file.path("jags", file),
-                              package = "Rbayesucsel",
-                              mustWork = TRUE)
-  lines <- readLines(file_package)
-  temp <- tempfile()
-  on.exit(unlink(temp, force = TRUE))
-  writeLines(lines, temp)
-  model <- R2jags::jags(
-    model.file = temp,
-    data = data,
-    parameters.to.save = parameters,
-    n.chains = 3,
-    n.iter = 8e3,
-    quiet = TRUE,
-    DIC = TRUE,
-    RNGname = "Wichmann-Hill",
-    inits = NULL
-  )
-  model
+  parameters_poisson <-  c("mu", "lambda", "p")
+  parameters_zip <- c("lambda", "p", "pi")
+  parameters_nb <- c("lambda", "c", "p")
+  fit_model <- function(file_name, parameters) {
+    data$n_obs <- length(data$yobs)
+    data$n_valdata <- length(data$ystar)
+    file_path <- system.file(file.path("jags", file_name),
+                             package = "Rbayesucsel",
+                             mustWork = TRUE)
+    lines <- readLines(file_path)
+    temp <- tempfile()
+    writeLines(lines, temp)
+    on.exit(unlink(temp, force = TRUE))
+    model <- R2jags::jags(
+      model.file = temp,
+      data = data,
+      parameters.to.save = parameters,
+      n.chains = 3,
+      n.iter = 8e3,
+      quiet = TRUE,
+      DIC = TRUE,
+      RNGname = "Wichmann-Hill",
+      inits = NULL
+    )
+  }
+  poisson_model <- fit_model("underreported_poisson.jags", parameters_poisson)
+  zip_model <- fit_model("underreported_zip.jags", parameters_zip)
+  nb_model <- fit_model("underreported_nb.jags", parameters_zip)
+  list(poisson = poisson_model,
+       zip = zip_model,
+       negbinom = nb_model)
 }
