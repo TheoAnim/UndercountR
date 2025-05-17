3466d13c6fe392f05aec06cd52c0aaed276b69c0
Theo Anim <theophilus.animbediako@jacks.sdstate.edu>
2025-05-17 18:13:15 +0100

    prior inputs

--- R/urc_mcmc.R
+++ R/urc_mcmc.R
@@ -2,12 +2,20 @@
 #' @export
 #' @param data A named list of data to pass to JAGS
 #' @param thresh decision rule for choosing parsimonious model when DICs are very close
-#' @param lambda_a shape for prior on expected number of true counts, lambda
-#' @param lambda_b rate for prior on expected number of true counts, lambda
-#' @return A list with model outputs, DICs and model with smallest DIC
+#' @param den_lambda prior on expected true counts, lambda
+#' @param den_p prior on reporting probability, p
+#' @param den_pi prior on zero-inflated parameter, pi of zip
+#' @param den_c prior on dispersion parameter, c of negbinom
+#' @return A named list of fitted models, DICs and model with smallest DIC
 
 
-urc_mcmc <- function(data, thresh = 2, lambda_a = 0.1, lambda_b = 0.1) {
+
+urc_mcmc <- function(data,
+                     thresh = 2,
+                     den_lambda = "dgamma(0.1,  0.1)",
+                     den_c = "dunif(0, 20)",
+                     den_p = "dunif(0,1)",
+                     den_pi = "dunif(0, 1)") {
   #setup to parallize
   future::plan(future::multisession)
   
@@ -15,27 +23,32 @@ urc_mcmc <- function(data, thresh = 2, lambda_a = 0.1, lambda_b = 0.1) {
   parameters_poisson <-  c("mu", "lambda", "p")
   parameters_zip <- c("lambda", "p", "pi")
   parameters_nb <- c("lambda", "c", "p")
-
-
+  
+  
   #function to fit each model
   fit_model <- function(file_name, parameters) {
     data$n_obs <- length(data$yobs)
     data$n_valdata <- length(data$ystar)
-    #jags require prior dist inputs as part of the datalist
-    data$lambda_a <- lambda_a
-    data$lambda_b <- lambda_b
-    density_lambda <- "dgamma(lambda_a, lambda_b)"
-
+    
     file_path <- system.file(file.path("jags", file_name),
                              package = "Rbayesucsel",
                              mustWork = TRUE)
     lines <- readLines(file_path)
-    lines <- gsub(
-      pattern = "prior_lambda",
-      replacement = density_lambda,
-      x = lines,
-      fixed = TRUE
-    )
+    #alter priors in jags.txt
+    if (grepl("nb", file_name)) {
+      lines <- gsub(
+        pattern = "prior_c",
+        replacement = den_c,
+        x = lines,
+        fixed = TRUE
+      )
+    } else if (grepl("zip", file_name)) {
+      lines <- gsub("prior_pi", den_pi, lines, fixed = TRUE)
+    }
+    lines <- stringr::str_replace_all(lines, 
+                                      c("prior_lambda" = den_lambda, 
+                                        "prior_p" = den_p))
+    
     temp <- tempfile()
     on.exit(unlink(temp, force = TRUE))
     writeLines(lines, temp)
@@ -51,11 +64,15 @@ urc_mcmc <- function(data, thresh = 2, lambda_a = 0.1, lambda_b = 0.1) {
       inits = NULL
     )
   }
-
-  model_files <- c("underreported_poisson.jags",
-                   "underreported_zip.jags",
-                   "underreported_nb.jags")
+  
+  model_files <- c(
+    "underreported_poisson.jags",
+    "underreported_zip.jags",
+    "underreported_nb.jags"
+  )
+  
   model_params <- list(parameters_poisson, parameters_zip, parameters_nb)
+  
   # Parallel model fitting
   model_outputs <- purrr::map2(model_files, model_params, fit_model)
   model_names <- c("poisson", "zip", "negbinom")
