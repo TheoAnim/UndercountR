3b41eaf85517252a84544ae6f8e80e8ccadfdb15
Theo Anim <theophilus.animbediako@jacks.sdstate.edu>
2025-05-17 16:49:34 +0100

    util modelchoice

--- R/urc_mcmc.R
+++ R/urc_mcmc.R
@@ -1,35 +1,10 @@
 #' @title model fitting for underreporting count data
-#' @export urc_mcmc
+#' @export
 #' @param data A named list of data to pass to JAGS
 #' @param thresh decision rule for choosing parsimonious model when DICs are very close
 #' @param lambda_a shape for prior on expected number of true counts, lambda
 #' @param lambda_b rate for prior on expected number of true counts, lambda
 #' @return A list with model outputs, DICs and model with smallest DIC
-
-
-model_choice <- function(df, thresh) {
-  # Ensure the tibble has the expected structure
-  if (!all(c("model_names", "DIC") %in% colnames(df))) {
-    stop("The tibble must have columns 'model_names' and 'DIC'")
-  }
-  min_dic <- min(df$DIC)
-
-  #models within 2 units of the min DIC
-  candidate_models <- df$model_names[df$DIC - min_dic <= thresh]
-  # If there are competing models, choose based on parsimony
-  if (length(candidate_models) > 1 &
-      "poisson" %in% candidate_models) {
-    # If poisson is among candidates, choose it (most parsimonious)
-    "poisson"
-  }
-  # If both zip and negbinom are candidates (equally complex), return the one with lower DIC
-  else {
-    candidate_dics <- df$DIC[df$model_names %in% candidate_models]
-    candidate_names <- df$model_names[df$model_names %in% candidate_models]
-    candidate_names[which.min(candidate_dics)]
-  }
-}
-
 
 
 urc_mcmc <- function(data, thresh = 2, lambda_a = 0.1, lambda_b = 0.1) {
