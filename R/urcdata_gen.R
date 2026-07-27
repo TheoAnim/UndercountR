e1efc904f639e61c21d4807791c408a6422b287f
Theo Anim <theophilus.animbediako@jacks.sdstate.edu>
2026-07-26 22:46:46 -0500

    external packages

--- R/urcdata_gen.R
+++ R/urcdata_gen.R
@@ -2,17 +2,17 @@
 #' @export
 urcdata_gen <- function(lambda, p, pi = 0, c = 1, nobs = 100, nv = 0, fun) {
   if (fun == "rpois") {
-    ystar <- rpois(nv, lambda)
-    yv <- rbinom(nv, ystar, p)
-    y <- rpois(nobs, lambda * p)
+    ystar <- stats::rpois(nv, lambda)
+    yv <- stats::rbinom(nv, ystar, p)
+    y <- stats::rpois(nobs, lambda * p)
   } else if (fun == "rzip") {
     ystar <- bizicount::rzip(nv, lambda, pi)
-    yv <- rbinom(nv, size = ystar, prob = p)
+    yv <- stats::rbinom(nv, size = ystar, prob = p)
     y <- bizicount::rzip(nobs, lambda * p, pi)
   } else if (fun == "rnegbin") {
-    ystar <- rnbinom(nv, mu = lambda, size = c)
-    yv <- rbinom(nv, size = ystar, prob = p)
-    y <- rnbinom(nobs, mu = lambda * p, size = c)
+    ystar <- stats::rnbinom(nv, mu = lambda, size = c)
+    yv <- stats::rbinom(nv, size = ystar, prob = p)
+    y <- stats::rnbinom(nobs, mu = lambda * p, size = c)
   } else {
     stop("fun must be rpois, rzip or rnegbin", call. = FALSE)
   }
